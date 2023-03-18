cbb3d3667576624ac084eeec937ca7f540c19b58
Guilherme Hermes <guilherme.hermes.053@ufrn.edu.br>
2023-03-18 13:57:56 -0300

    Terminando madlibs

---
+++ madlibs.py
@@ -0,0 +1,30 @@
+def madlibs():
+
+    adj = input("Adjective: ")
+    adj2 = input("Adjective: ")
+    adj3 = input("Adjective: ")
+    adj4 = input("Adjective: ")
+    verb = input("Verb: ")
+    verb2 = input("Verb: ")
+    profession = input("Profession: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    city = input("City: ")
+    fairy_tale = input("Fairy tale: ")
+
+
+    madlib = f" Romeo was the only son of the {adj} family in {city}.\
+He was to {verb} the daughter of the next town’s most \
+{adj4} family for business. But Romeo was in a relationship \
+with their {profession}'s daughter, Juliet. After hearing the news \
+of Romeo’s arranged wedding, she lost all {noun1}. Just like the {adj2} \
+{fairy_tale}, she clearly knew what they had was not something to be.\
+However, Romeo had the courage to {verb2} for their {noun2}. \
+His parents were completely against them. They value power \
+and wealth more than their own son’s happiness. \
+Romeo’s father threatened not to give him any inheritance \
+and cut off their ties if he chooses Juliet. \
+In the end, Romeo chose Juliet. He knew he can’t be {adj3} \
+being wealthy without Juliet."
+    print(madlib)
+
