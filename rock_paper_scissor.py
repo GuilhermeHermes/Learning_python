fad924d3a18dd3f9c2ec329553de4dd5c611a33f
Guilherme Hermes <guilherme.hermes.053@ufrn.edu.br>
2023-03-29 19:53:01 -0300

    Update rock_paper_scissor.py
    Colocando exceções.

--- rock_paper_scissor.py
+++ rock_paper_scissor.py
@@ -3,7 +3,14 @@ import random
 def play():
         play_again = 1
         while play_again == 1:
-            user = input("rock(R), paper(P), Scissor(S): ".lower())
+            while True:
+                try:
+                    user = input("rock(R), paper(P), Scissor(S): ").lower()
+                    if user not in ['r', 'p', 's']:
+                        raise ValueError("Invalid input. Please enter 'r', 'p', or 's'.")
+                    break
+                except ValueError as error:
+                    print(error)
             computer = random.choice(['r', 'p', 's'])
             if(user==computer):
                 print(f"the computer select {computer}, tie!")
@@ -11,6 +18,12 @@ def play():
                 print(f" the computer select {computer}, You won!")
             else:
                 print(f"the computer select {computer}, you lost!")
-
-            play_again = int(input("Do you wanna play again?(1)YES (2)NOT: "))
+            while True:
+                try:
+                    play_again = int(input("Do you wanna play again?(1)YES (2)NOT: "))
+                    if play_again not in (1,2):
+                         raise ValueError("Invalid input. Please enter (1)YES (2)NOT: ")
+                    break
+                except ValueError as error:
+                    print(error)
 play()
